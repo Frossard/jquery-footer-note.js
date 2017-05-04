7e6ae7eac7bf8bcf5cf12b3dae960964905ca70f
Victor Frossard <victorfrossard00@gmail.com>
2017-05-03 22:27:53 -0300

    Html legend and scroll-y to aside.

--- nota-rodape.js
+++ nota-rodape.js
@@ -1,6 +1,6 @@
 $(function(){
     $(document).ready(function(){
-        $( "<div class='blackboard'></div><aside class='footer-note'><div class='container'><div class='row'><div id='target-legend' class='col-md-12'><p>Teste</p></div></div></div></aside>" ).appendTo('body');
+        $( "<div class='blackboard'></div><aside class='footer-note'><div class='container'><div class='row'><div id='target-legend' class='col-md-12'></div></div></div></aside>" ).appendTo('body');
         
         $('.blackboard').click(function(){
             $(this).fadeOut(200);
@@ -12,13 +12,16 @@ $(function(){
         e.preventDefault();
         
         /* Recuperando a legenda */
-        var legenda = $(this).data('legend');
+        var legendaId = $(this).data('legend');
+        var legenda = $(legendaId).html();
         /* Recuperando a palavra destacada */
         var palavra = $(this).text();
         
-        $('#target-legend > p').html("<strong>" + palavra + "</strong> — " + legenda + "</p>");
+        $('#target-legend').html("<strong>" + palavra + "</strong> — " + legenda + "</p>");
         $('.blackboard').fadeIn(200);
         $('.footer-note').slideDown(200);
+        
+        /* Rolagem */
     });
     
     
